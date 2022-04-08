9252bfd721b6984a593a6467f2fa76688142a700
babuharsh19 <babuharsh19@gmail.com>
2022-04-08 14:19:51 +0530

    initial commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ login_manager.login_message_category = "info"
 login_manager.login_view = 'login'
 
 active = 'active'
-local_server =True
+local_server =False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 if(local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/brilliant_zone'
@@ -118,7 +118,7 @@ def events():
 # @login_required
 def notice():
     return render_template("notice.html",active=active)
-# if __name__ == "__main__":
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=8080)
-app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+# app.run(debug=True)
